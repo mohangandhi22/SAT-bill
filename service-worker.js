654b82f6846a400466c3542356561755587e8a26
sat bill <mohangandhi96007@gmail.com>
2026-04-03 18:39:23 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = "sat-bill-v5";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// INSTALL
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// ACTIVATE
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// FETCH (OFFLINE FIRST)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => {
+        // fallback (optional)
+        return caches.match("./index.html");
+      });
+    })
+  );
+});
